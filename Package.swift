582d4909778fa679f673de01e405ac06e484e087
ci <ci@banuba.com>
2023-11-24 17:07:42 +0300

    1.9.1-86-g65c1c833d4

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+
+let package = Package(
+    name: "BNBResources",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBResources",
+            targets: [
+                "BNBResources"
+            ]
+        ),
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBResources",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBResources.zip",
+            checksum: "77ec8f006482e31f02da542dff67a9e4aeef0d0cc941ab509233d96295bed3fb"
+        ),
+    ]
+)

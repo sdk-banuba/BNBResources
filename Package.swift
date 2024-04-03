6837627100f5956ea76c5dcab6f0214718dcbe52
ci <ci@banuba.com>
2024-04-03 11:36:33 +0300

    1.12.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.11.1-70-g9f1f1a3b1"
+let bnbPackageVersion: Version = "1.12.0"
 
 let package = Package(
     name: "BNBResources",
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBResources",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.1-70-g9f1f1a3b1/BNBResources.zip",
-            checksum: "d28a9a254e79e7730778a7dbe3294015382e45f97d743fa4441b775d52f436d0"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.12.0/BNBResources.zip",
+            checksum: "e6cfd3b5bd79d754bde6026063560ec4eb0613c22afa466afd6d91e6a4046c97"
         ),
     ]
 )
